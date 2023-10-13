8be7e51481a759ff4f2938e8b1a7f615a080e7ca
yawarhussain7 <117814780+yawarhussain7@users.noreply.github.com>
2023-10-13 15:12:37 +0530

    In Process

---
+++ Quardatic_Equation.c
@@ -0,0 +1,40 @@
+/*
+Author: Yawar-Husain
+
+General form    ax^2 + bx + c
+Equation  :  -b -+ sqrt( b^2 - 4ac )/2a
+
+You must explicitly include math.h library in c using (" -lm " ) flage
+
+ gcc file_name.c -lm
+*/
+#include<stdlib.h>
+#include<stdio.h>
+#include<math.h>
+
+int main(int argc, char **argv)
+{
+    double a, b, c;
+
+    printf("Please enter value of a \n:");
+    scanf("%lf",&a);
+
+    printf("Please enter value of b \n:");
+    scanf("%lf",&b);
+
+    printf("Please enter value of c \n:");
+    scanf("%lf",&c);
+
+    double root1 = ( -b + sqrt( pow(b, 2) - 4 * a *c )/(2 * a));
+    double root2 = ( -b + sqrt( pow(b, 2) - 4 * a *c )/(2 * a));
+
+    printf("The root of %dx^2 + %dx + %d are %f and %f \n",a,b,c,root1,root2);
+
+    // int a / 0 ;         //Give warning it crashed over program
+    int y = 0;
+    int x = x / y;           //it will not give warning because value is stored in integer but program crashed
+
+    printf("Almost done ....\n");
+
+    return 0;
+}
