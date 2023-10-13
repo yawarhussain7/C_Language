0b0bd2effe83764fbc58e92feada6e35afb31ff1
yawarhussain7 <117814780+yawarhussain7@users.noreply.github.com>
2023-10-13 20:50:36 +0530

    COmplete Program

--- Quardatic_Equation.c
+++ Quardatic_Equation.c
@@ -16,25 +16,37 @@ int main(int argc, char **argv)
 {
     double a, b, c;
 
-    printf("Please enter value of a \n:");
-    scanf("%lf",&a);
+    if(argc != 4)
+    {
+        printf("ERROR : You must input 4 number...! \n");
+        exit(1);
+    }
+    //Get input on run time
+    a = atof(argv[1]);                    //atoi(): argument ot integer
+    b = atof(argv[2]);                    // [a] stand for alpha-numeric and [i] stand for integer
+    c = atof(argv[3]);
 
-    printf("Please enter value of b \n:");
-    scanf("%lf",&b);
+    //Get input from user :
 
-    printf("Please enter value of c \n:");
-    scanf("%lf",&c);
+    // printf("Please enter value of a :\n");
+    // scanf("%lf",&a);
+
+    // printf("Please enter value of b :\n");
+    // scanf("%lf",&b);
+
+    // printf("Please enter value of c :\n");
+    // scanf("%lf",&c);
 
     double root1 = ( -b + sqrt( pow(b, 2) - 4 * a *c )/(2 * a));
     double root2 = ( -b + sqrt( pow(b, 2) - 4 * a *c )/(2 * a));
 
-    printf("The root of %dx^2 + %dx + %d are %f and %f \n",a,b,c,root1,root2);
+    printf("The root of %.3fx^2 + %.4fx + %.2f are %f and %f \n",a,b,c,root1,root2);
 
-    // int a / 0 ;         //Give warning it crashed over program
-    int y = 0;
-    int x = x / y;           //it will not give warning because value is stored in integer but program crashed
+    // // int a / 0 ;         //Give warning it crashed over program
+    // int y = 0;
+    // int x = x / y;           //it will not give warning because value is stored in integer but program crashed
 
-    printf("Almost done ....\n");
+    // printf("Almost done ....\n");
 
     return 0;
 }
